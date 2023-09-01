099180ac9d390767624a6aa38536eb04b7b697bb
cuma561 <krawczakbartek@gmail.com>
2023-09-01 18:35:42 +0200

    Add style to Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,10 @@
 import { Button } from '../Button/Button';
+import styles from './Form.module.css'
 export function Form()
 {
 	return(
-		<form>
-			<input type="text" placeholder="Wpisz nazwe zadania"/>
+		<form className={styles.form}>
+			<input className={styles.input} type="text" placeholder="Wpisz nazwe zadania"/>
 			<Button>Dodaj</Button>
 		</form>
 	)
