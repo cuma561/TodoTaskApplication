cdf39b5a0a56361ff3b70c989c38a7ce57ccecb9
cuma561 <krawczakbartek@gmail.com>
2023-09-01 17:57:35 +0200

    Form component
    - add new component -> Form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,10 @@
+import { Button } from '../Button/Button';
+export function Form()
+{
+	return(
+		<form>
+			<input type="text" placeholder="Wpisz nazwe zadania"/>
+			<Button>Dodaj</Button>
+		</form>
+	)
+}
