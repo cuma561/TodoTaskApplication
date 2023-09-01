246feb38945d16c58d72306932eb6ebffdfe3232
cuma561 <krawczakbartek@gmail.com>
2023-09-01 18:47:41 +0200

    Remove reload form submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,10 @@
 import { Button } from '../Button/Button';
-import styles from './Form.module.css'
+import styles from './Form.module.css';
+
 export function Form()
 {
 	return(
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={(event) => {event.preventDefault();}}>
 			<input className={styles.input} type="text" placeholder="Wpisz nazwe zadania"/>
 			<Button>Dodaj</Button>
 		</form>
