510e016972b485cbae6a96b9a1ff654ffa7ba80c
cuma561 <krawczakbartek@gmail.com>
2023-09-01 18:01:58 +0200

    Add content todo application

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Form } from './components/Form/Form';
+import { TodoItem } from './components/TodoItem/TodoItem';
+
 function App() {
 
   return (
     <>
-      
+      <div>
+        <header>
+          <div>
+            <h1>Zadania do zrobienia</h1>
+            <h2>5 zadań</h2>
+            <button>+</button>
+          </div>
+        </header>
+        <Form />
+        <ul>
+          <TodoItem />
+        </ul>
+      </div>
     </>
   )
 }
