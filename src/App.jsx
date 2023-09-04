9bfe24a69789536998727636f196ab76efb242f0
cuma561 <krawczakbartek@gmail.com>
2023-09-04 21:12:27 +0200

    MainTodo components
    - add new component -> main todo
    - moving code with App.jsx to MainTodo.jsx
    - moving style App.module.css to MainTodo.module.css
    - add new component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,13 @@
-import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import styles from './App.module.css'
 
-import { Form } from './components/Form/Form';
-import { TodoItem } from './components/TodoItem/TodoItem';
-import { Header } from './components/Header/Header';
-import { ShowFormButton } from './components/Button/ShowFormButton';
-import { Subheading } from './components/Subheading/Subheading';
-
-import { getSubheading } from "./utils/getSubheading";
+import { MainTodo } from './components/MainTodo/MainTodo';
 
 function App() {
 
-  const localStorageTodos = JSON.parse(localStorage.getItem("todos"));
-
-  const [isShowForm, setIsShowForm] = useState(false);
-
-  const [todos, setTodos] = useState(localStorageTodos);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  function addTodo(newTodoName)
-  {
-    setTodos((prevTodos) => [
-      ...prevTodos,
-      {
-        id: prevTodos.length + 1,
-        name: newTodoName,
-        done: false
-      },
-    ]);
-    setIsShowForm(false);
-  }
-
-  function deleteTodo(id)
-  {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-  }
-
-  function doneTodo(id) {
-        setTodos((prevTodos) =>
-            prevTodos.map((todo) => {
-                if (todo.id !== id) {
-                    return todo;
-                }
-
-                return {
-                    ...todo,
-                    done: true,
-                };
-            })
-        );
-    }
-
-    function undoneTodo(id) {
-      setTodos((prevTodos) =>
-            prevTodos.map((todo) => {
-                if (todo.id !== id) {
-                    return todo;
-                }
-
-                return {
-                    ...todo,
-                    done: false,
-                };
-            })
-        );
-    }
-
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <Header name="Todo Tasks"/>
-          {!isShowForm && <ShowFormButton name="+" onClick={() => setIsShowForm(true)}/>}
-        </div>
-        <Subheading todos={getSubheading(todos.length)}/>
-        {isShowForm && <Form onFormSubmit={(newTodoName) => addTodo(newTodoName)} />}
-        <ul>
-          {todos.map(({ id, name, done }) => (
-            <TodoItem key={id} name={name} done={done} onDone={() => doneTodo(id)} 
-              onDelete={() => deleteTodo(id)} onUndone={() => undoneTodo(id)} />
-          ))}
-        </ul>
-      </div>
+      <MainTodo />
     </>
   )
 }
---
+++ src/components/MainTodo/MainTodo.jsx
@@ -0,0 +1,91 @@
+import styles from './MainTodo.module.css'
+import { useEffect, useState } from 'react'
+import { Header } from '../Header/Header';
+import { ShowFormButton } from '../Button/ShowFormButton';
+
+import { Subheading } from '../Subheading/Subheading';
+
+import { getSubheading } from "../../utils/getSubheading";
+
+import { Form } from '../Form/Form';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+export function MainTodo()
+{
+	const localStorageTodos = JSON.parse(localStorage.getItem("todos"));
+
+  	const [todos, setTodos] = useState(localStorageTodos);
+
+	const [isShowForm, setIsShowForm] = useState(false);
+
+	useEffect(() => {
+    	localStorage.setItem("todos", JSON.stringify(todos));
+  	}, [todos]);
+
+  	function addTodo(newTodoName)
+  	{
+    	setTodos((prevTodos) => [
+      		...prevTodos,
+      		{
+        		id: prevTodos.length + 1,
+        		name: newTodoName,
+        		done: false
+      		},
+    	]);
+    	setIsShowForm(false);
+  	}
+
+  	function deleteTodo(id)
+  	{
+    	setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  	}
+
+  	function doneTodo(id) {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => {
+                if (todo.id !== id) {
+                    return todo;
+                }
+
+                return {
+                    ...todo,
+                    done: true,
+                };
+            })
+        );
+    }
+
+    function undoneTodo(id) {
+      setTodos((prevTodos) =>
+            prevTodos.map((todo) => {
+                if (todo.id !== id) {
+                    return todo;
+                }
+
+                return {
+                    ...todo,
+                    done: false,
+                };
+            })
+        );
+    }
+
+	return(
+		<>
+			<div className={styles.container}>
+				<div className={styles.row}>
+					<Header name="Todo Tasks"/>
+					{!isShowForm && <ShowFormButton name="+" onClick={() => setIsShowForm(true)}/>}
+				</div>
+				<Subheading todos={getSubheading(todos.length)}/>
+				{isShowForm && <Form onFormSubmit={(newTodoName) => addTodo(newTodoName)} />}
+        		<ul>
+          			{todos.map(({ id, name, done }) => (
+            			<TodoItem key={id} name={name} done={done} onDone={() => doneTodo(id)} 
+              				onDelete={() => deleteTodo(id)} onUndone={() => undoneTodo(id)} />
+          			))}
+        		</ul>
+			</div>
+		</>
+	)
+}
