56dd8514f629ce34d9ba14213dba76990844c10c
cuma561 <krawczakbartek@gmail.com>
2023-09-05 08:37:48 +0200

    FormCategory components
    - add new component -> Form Category

---
+++ src/components/Form/FormCategory.jsx
@@ -0,0 +1,17 @@
+import { Button } from '../Button/Button';
+import styles from './Form.module.css';
+import { useState } from 'react'; 
+
+export function FormCategory({ onFormSubmit })
+{
+	const [isValue, setIsValue] = useState("");
+
+	return(
+		<form className={styles.form} 
+			onSubmit={(event) => {event.preventDefault(); onFormSubmit(isValue);}}>
+			<input value={isValue} onChange={(event) => setIsValue(event.target.value)} 
+				className={styles.input} type="text" placeholder="Enter the name of the category task"/>
+			<Button disabled={isValue === ""}>Add</Button>
+		</form>
+	)
+}
