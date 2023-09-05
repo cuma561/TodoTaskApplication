8fcccd8fad1b82e7cce6a7ffdc2e1f3ba63b56ef
cuma561 <krawczakbartek@gmail.com>
2023-09-05 09:38:50 +0200

    Change menu application
    - add default link and remove section element

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -3,15 +3,16 @@ import { NavLink } from 'react-router-dom'
 export function MainMenu()
 {
 	return(
-		<section>
-			<ul>
-				<li>
-					<NavLink to="/todos">Todo Task</NavLink>
-				</li>
-				<li>
-					<NavLink to="/category">Category Task</NavLink>
-				</li>
-			</ul>
-		</section>
+		<ul>
+			<li>
+				<NavLink to="/">Homepage</NavLink>
+			</li>
+			<li>
+				<NavLink to="/todos">Todo Task</NavLink>
+			</li>
+			<li>
+				<NavLink to="/category">Category Task</NavLink>
+			</li>
+		</ul>
 	)
 }
