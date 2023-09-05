639db8d5d8c31fcc3c8d2f021b5313fd1fae70d3
cuma561 <krawczakbartek@gmail.com>
2023-09-05 09:23:28 +0200

    Add routing to application

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { MainTodo } from './components/MainTodo/MainTodo';
+import { MainCategory } from './components/MainCategory/MainCategory';
+
+const router = createBrowserRouter([
+  {
+    path: '/todos',
+    element: <MainTodo />
+  },
+  {
+    path: '/category',
+    element: <MainCategory />
+  },
+  {
+    path: '/',
+    element: <App />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
