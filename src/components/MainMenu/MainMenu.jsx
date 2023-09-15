c811ad1b6b97bed2228eb20760afdb4822155e43
cuma561 <krawczakbartek@gmail.com>
2023-09-15 22:05:08 +0200

    Refactor code
    - remove li element with menu app

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -3,15 +3,9 @@ import { NavLink } from "react-router-dom";
 export function MainMenu() {
   return (
     <ul className="menu">
-      <li>
-        <NavLink to="/">Homepage</NavLink>
-      </li>
-      <li>
-        <NavLink to="/todos">Todo Task</NavLink>
-      </li>
-      <li>
-        <NavLink to="/category">Category Task</NavLink>
-      </li>
+      <NavLink to="/">Homepage</NavLink>
+      <NavLink to="/todos">Todo Task</NavLink>
+      <NavLink to="/category">Category Task</NavLink>
     </ul>
   );
 }
