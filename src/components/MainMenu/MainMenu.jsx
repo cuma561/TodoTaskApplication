cee47047d14638e3cea81a0bd08e8d5051b32bf8
cuma561 <krawczakbartek@gmail.com>
2023-09-18 11:46:56 +0200

    Change ul element to div element in menu application

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 export function MainMenu() {
   return (
-    <ul className="menu">
+    <div className="menu">
       <NavLink to="/">Homepage</NavLink>
       <NavLink to="/todos">Todo Task</NavLink>
       <NavLink to="/category">Category Task</NavLink>
-    </ul>
+    </div>
   );
 }
